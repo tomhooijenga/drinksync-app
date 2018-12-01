d6f25a2e31a0d71f2cd8408a8f624f68c22d1bc0
Tom Hooijenga <tomhooijenga@gmail.com>
2018-12-01 12:39:15 +0100

    Improve sharing

---
+++ src/lib/share.js
@@ -0,0 +1,34 @@
+import store from "./store";
+
+export function shareGroup(group) {
+    const users = group
+        .users
+        .map(({id}) => store.state.users[id])
+        .sort((a, b) => {
+            return b.drinks - a.drinks
+        });
+
+    let text = `Join the group "${group.name}" to have some drinks`;
+    const [user1, user2, ...rest] = users;
+
+    if (user1) {
+        text += ` with ${user1.name}`;
+    }
+    if (user2) {
+        text += `${rest.length ? ',' : ' and'} ${user2.name}`
+    }
+    if (rest.length) {
+        text += ` and ${rest.length} more`
+    }
+    text += '!';
+
+    const share = navigator.share({
+        text,
+        title: `Join our group "${this.group.name}" on Drink Sync`,
+        url: window.location.href
+    });
+
+    share.then(() => gtag('event', 'share');
+
+    return share;
+}
